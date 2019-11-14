d576a58a7464b6f3f84a57d406b48bf3e110d6c8
Skiwa <haegman.julien@laposte.net>
2019-11-14 15:08:45 +0100

    Added routes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,41 +1,42 @@
-import React from 'react';
-import './App.css';
-import PortraitAdversaire from '../portraitAdversaire/PortraitAdversaire';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import ListeAdversaires from "../listeAdversaires/ListeAdversaires";
+import Login from "../login/Login";
 
-class AdversaireEntité {
-  constructor(photo, prenom, age, poids, taille, description, sports) {
-    this.photo = photo;
-    this.prenom = prenom;
-    this.age = age;
-    this.poids = poids;
-    this.taille = taille;
-    this.description = description;
-    this.sports = sports;
-  }
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/liste">Liste</Link>
+            </li>
+            <li>
+              <Link to="/login">login</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/liste">
+            <ListeAdversaires/>
+          </Route>
+          <Route path="/login">
+            <Login/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
-
-class App extends React.Component {
-  adversaires = [];
-
-  constructor(props) {
-    super(props);
-
-    this.adversaires.push(new AdversaireEntité('assets/img/alexandre.jpg','Alexandre',28,90,'1m80', 'J\'aime me battre', ['MMA', 'Krav Maga', 'Karaté']));
-    this.adversaires.push(new AdversaireEntité('assets/img/kaaris.jpg', 'Kaaris', 39, 100, '1m80', 'Je trempe mes cookies dans tes larmes.', ['MMA','Boxe']));
-    this.adversaires.push(new AdversaireEntité('assets/img/jean-pierre.jpg', 'Jean Pierre', 69, 76, '1m80', 'Coucou TF1.', ['Pétanque']));
-    this.adversaires.push(new AdversaireEntité('assets/img/laurent.jpg', 'Laurent', 56, 73, '1m83', 'Salut c\'est Laurent Ruquier.', ['Marche rapide']));
-  }
-
-  render() {
-    return (
-        <div className="row">
-          {this.adversaires.map((adversaire,index)=>{
-            return <PortraitAdversaire adversaire={adversaire}/>
-          })
-          }
-         </div>
-    );
-  }
-}
-
-export default App;
---
+++ src/components/listeAdversaires/ListeAdversaires.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PortraitAdversaire from '../portraitAdversaire/PortraitAdversaire';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+class AdversaireEntité {
+  constructor(photo, prenom, age, poids, taille, description, sports) {
+    this.photo = photo;
+    this.prenom = prenom;
+    this.age = age;
+    this.poids = poids;
+    this.taille = taille;
+    this.description = description;
+    this.sports = sports;
+  }
+}
+
+class ListeAdversaires extends React.Component {
+  adversaires = [];
+
+  constructor(props) {
+    super(props);
+
+    this.adversaires.push(new AdversaireEntité('assets/img/alexandre.jpg','Alexandre',28,90,'1m80', 'J\'aime me battre', ['MMA', 'Krav Maga', 'Karaté']));
+    this.adversaires.push(new AdversaireEntité('assets/img/kaaris.jpg', 'Kaaris', 39, 100, '1m80', 'Je trempe mes cookies dans tes larmes.', ['MMA','Boxe']));
+    this.adversaires.push(new AdversaireEntité('assets/img/jean-pierre.jpg', 'Jean Pierre', 69, 76, '1m80', 'Coucou TF1.', ['Pétanque']));
+    this.adversaires.push(new AdversaireEntité('assets/img/laurent.jpg', 'Laurent', 56, 73, '1m83', 'Salut c\'est Laurent Ruquier.', ['Marche rapide']));
+  }
+
+  render() {
+    return (
+        <div className="row">
+          {this.adversaires.map((adversaire,index)=>{
+            return <PortraitAdversaire adversaire={adversaire}/>
+          })
+          }
+         </div>
+    );
+  }
+}
+
+export default ListeAdversaires;

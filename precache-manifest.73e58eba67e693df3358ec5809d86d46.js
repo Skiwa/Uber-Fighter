1d6ff27bbb93017b88e393827f1c4613a6402588
Skiwa <haegman.julien@laposte.net>
2019-12-16 22:41:52 +0100

    Updates

---
+++ precache-manifest.73e58eba67e693df3358ec5809d86d46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efdba8a33660055732825a2723743a1e",
+    "url": "/Skiwa/Uber-Fighter/index.html"
+  },
+  {
+    "revision": "4444aa7bf343d885e797",
+    "url": "/Skiwa/Uber-Fighter/static/css/main.e6dcaed9.chunk.css"
+  },
+  {
+    "revision": "71db3e0e0d8914411bb0",
+    "url": "/Skiwa/Uber-Fighter/static/js/2.37483b54.chunk.js"
+  },
+  {
+    "revision": "4444aa7bf343d885e797",
+    "url": "/Skiwa/Uber-Fighter/static/js/main.bf659c6d.chunk.js"
+  },
+  {
+    "revision": "7d9e1865fe20201f5d56",
+    "url": "/Skiwa/Uber-Fighter/static/js/runtime-main.bf9d2755.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Skiwa/Uber-Fighter/precache-manifest.73e58eba67e693df3358ec5809d86d46.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Skiwa/Uber-Fighter/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
